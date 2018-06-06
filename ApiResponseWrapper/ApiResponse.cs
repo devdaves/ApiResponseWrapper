7d0ea3b4da942840bb45931e3a653956cb865030
Dave Stensgaard <info@devdave.com>
2018-06-06 16:42:23 -0700

    add initial response and error classes

---
+++ ApiResponseWrapper/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ApiResponseWrapper
+{
+    public class ApiResponse<T> where T: class
+    {
+        public string RequestId { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public T Result { get; set; }
+
+        public ApiError Error { get; set; }
+
+        public ApiResponse(HttpStatusCode statusCode, string requestId, T result = null, ApiError error = null)
+        {
+            StatusCode = (int)statusCode;
+            RequestId = requestId;
+            Result = result;
+            Error = error;
+        }
+    }
+}
